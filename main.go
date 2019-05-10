cfd21f93bdba980e1b63bf5c1edef6b3c7ad164a
Stefan Engstrom <sengstrom@ena.com>
2019-05-09 17:24:54 -0700

    now hu-mon readable

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/nomad/jobspec"
 	"github.com/myENA/consultant"
 	"github.com/myENA/consultant/util"
-	"github.com/rs/zerolog"
 	"math/rand"
 	"net/url"
 	"os"
@@ -19,15 +18,11 @@ import (
 	"time"
 )
 
-var log = zerolog.New(os.Stdout).
-	With().Timestamp().Str("service", "local-service").
-		Logger().Level(zerolog.InfoLevel)
-
 var (
 	fs          = flag.NewFlagSet("local_service", flag.ExitOnError)
 	jobspecFile = fs.String("jobspec", "nomad-jobspec.tmpl", "Specify jobspec to use if not default")
-	iface = fs.String("iface", "", "Specify address by interface")
-	debug = fs.Bool("debug",false,"Add extra logging")
+	iface       = fs.String("iface", "", "Specify address by interface")
+	debug       = fs.Bool("debug", false, "Add extra logging")
 )
 
 func init() {
@@ -38,80 +33,79 @@ func main() {
 
 	err := fs.Parse(os.Args[1:])
 	if err != nil {
-		log.Fatal().Msgf("Error parsing flags: %v", err)
-	}
-	if *debug {
-		log.Level(zerolog.DebugLevel)
+		fmt.Printf("Error parsing flags: %v\n", err)
 	}
 
 	consul, err := consultant.NewDefaultClient()
 	if err != nil {
-		log.Fatal().Msgf("Couldn't build a consul client, that's a real problem: %v", err)
+		fmt.Printf("Couldn't build a consul client, that's a real problem: %v\n", err)
+		os.Exit(1)
 	}
 	job, err := jobspec.ParseFile(*jobspecFile)
 	if err != nil {
-		log.Fatal().Msgf("Error when trying to parse %s: %v", jobspecFile, err)
+		fmt.Printf("Error when trying to parse %s: %v\n", jobspecFile, err)
+		os.Exit(1)
 	}
 
 	taskGroups := job.TaskGroups
 
-	myAddress,err := util.MyAddress()
+	myAddress, err := util.MyAddress()
 	if err != nil {
-		log.Fatal().Msgf("Unable to determine the address of this host: %s", err)
+		fmt.Printf("Unable to determine the address of this host: %s\n", err)
+		os.Exit(1)
 	}
 
 	for k := range taskGroups {
-		log.Info().Msgf("Processing task group: %s", *taskGroups[k].Name)
+		fmt.Printf("Processing task group: %s\n", *taskGroups[k].Name)
 		taskList := taskGroups[k].Tasks
 		for l := range taskList {
-			log.Info().Msgf("Diving into task list: %s", taskList[l].Name)
+			fmt.Printf("> Diving into task list: %s\n", taskList[l].Name)
 			serviceList := taskList[l].Services
 			for _, s := range serviceList {
-				log.Info().Msgf("Looking at service: %s", s.Name)
+				fmt.Printf("> > Looking at service: %s\n", s.Name)
 
 				port, err := strconv.Atoi(s.PortLabel)
 				if err != nil {
-					log.Warn().Msgf("Service port label not a number (%s): %s", s.PortLabel, err)
+					fmt.Printf("> > Service port label not a number (%s): %s\n", s.PortLabel, err)
 					continue
 				}
 
 				checks := buildChecks(s, myAddress)
 
 				asr := &capi.AgentServiceRegistration{
-					ID: serviceId(s.Name),
-					Name: s.Name,
-					Tags: s.Tags,
-					Port: port,
+					ID:      serviceId(s.Name),
+					Name:    s.Name,
+					Tags:    s.Tags,
+					Port:    port,
 					Address: myAddress,
-					Checks: checks,
+					Checks:  checks,
 				}
 
-				j,_ := json.Marshal(asr)
-				// this is clearly not the correct way to use zerolog
-				log.Info().RawJSON("payload",j).Msg("write")
+				j, _ := json.MarshalIndent(asr,"> > > ","  ")
+				fmt.Printf("> > Registration: %s\n",string(j))
 
 				err = consul.Agent().ServiceRegister(asr)
 				defer func(id string) {
 					err := consul.Agent().ServiceDeregister(id)
 					if err != nil {
-						log.Info().Msgf("Error deregistering %s",id)
+						fmt.Printf("Error deregistering %s\n", id)
 					} else {
-						log.Info().Msgf("Deregistered %s",id)
+						fmt.Printf("Deregistered %s\n", id)
 					}
 				}(asr.ID)
 				if err != nil {
-					log.Warn().Msgf("Something went wrong when trying to register the service: %v",err)
+					fmt.Printf("> ? Something went wrong when trying to register the service: %v\n", err)
 				}
 			}
 		}
 	}
 
 	// Wait for a signal to exit
-	log.Info().Msg("Now just waiting for the end")
+	fmt.Println("Now just waiting for the end")
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	sig := <-ch
-	log.Info().Msgf("Got a signal (%s), exiting\n", sig)
+	fmt.Printf("Got a signal (%s), exiting\n", sig)
 
 }
 
@@ -123,14 +117,14 @@ func buildChecks(s *napi.Service, myAddress string) []*capi.AgentServiceCheck {
 	checks := capi.AgentServiceChecks{}
 	for _, c := range s.Checks {
 		asc := &capi.AgentServiceCheck{
-			CheckID: c.Id,
-			Name: c.Name,
+			CheckID:  c.Id,
+			Name:     c.Name,
 			Interval: c.Interval.String(),
 		}
 
 		checkPort, err := strconv.Atoi(c.PortLabel)
 		if err != nil {
-			log.Warn().Msgf("Check port is not a number (%s): %v", c.PortLabel, err)
+			fmt.Printf("> > > Check port is not a number (%s): %v\n", c.PortLabel, err)
 			continue
 		}
 
@@ -138,12 +132,12 @@ func buildChecks(s *napi.Service, myAddress string) []*capi.AgentServiceCheck {
 		case "http":
 			u := url.URL{
 				Scheme: "http",
-				Host: fmt.Sprintf("%s:%d", myAddress, checkPort),
-				Path: c.Path,
+				Host:   fmt.Sprintf("%s:%d", myAddress, checkPort),
+				Path:   c.Path,
 			}
 			asc.HTTP = u.String()
 		default:
-			log.Warn().Msgf("Unhandled check method: %s", c.Method)
+			fmt.Printf("> > > Unhandled check method: %s\n", c.Method)
 		}
 
 		checks = append(checks, asc)
